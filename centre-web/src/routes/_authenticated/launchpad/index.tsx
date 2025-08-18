e30487b43a86099daaad5907576d5d66e5ed2bee
Jad <jad.saad@aot-technologies.com>
2025-08-18 16:50:53 -0700

    feat: Integrate LaunchAppTile into launchpad page

--- centre-web/src/routes/_authenticated/launchpad/index.tsx
+++ centre-web/src/routes/_authenticated/launchpad/index.tsx
@@ -1,4 +1,6 @@
+import { List as EpicTileList } from "@/components/LaunchAppTile/List";
 import { PageGrid } from "@/components/Shared/PageGrid";
+import { EpicApp } from "@/models/EpicApp";
 import { Grid } from "@mui/material";
 import { createFileRoute } from "@tanstack/react-router";
 
@@ -7,9 +9,45 @@ export const Route = createFileRoute("/_authenticated/launchpad/")({
 });
 
 function Launchpad() {
+  const mockEngageTile = {
+    app_id: 1,
+    name: "EPIC.engage",
+    description: "Public Engagement Application",
+    launch_url: "https://engage.eao.gov.bc.ca/",
+    is_active: true,
+    user: {
+      access_level: "Super User",
+      last_accessed: "2025-07-07",
+      custom_order: 2,
+      bookmarks: [
+        {
+          label: "Yellowhead",
+          url: "https://engage.eao.gov.bc.ca/Yellowhead-EE",
+        },
+      ],
+    },
+  };
+  const mockTiles: EpicApp[] = [
+    mockEngageTile,
+    {
+      app_id: 2,
+      name: "EPIC.submit",
+      description: "Document submission Tool for Proponents and Holders",
+      launch_url: "https://submit.eao.gov.bc.ca/",
+      is_active: true,
+      user: {
+        access_level: null,
+        last_accessed: null,
+        custom_order: null,
+        bookmarks: [],
+      },
+    },
+  ];
   return (
     <PageGrid>
-      <Grid item>Hello /_authenticated/launchpad/!</Grid>
+      <Grid item>
+        <EpicTileList items={mockTiles} />
+      </Grid>
     </PageGrid>
   );
 }
