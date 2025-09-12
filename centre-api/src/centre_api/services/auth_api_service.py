6fb9b34dcd7ce1b3a8c771ebb40b7d91b98b27cf
Jad <jad.saad@aot-technologies.com>
2025-09-12 16:24:44 -0700

    chore(lint): D400 First line should end with a period

--- centre-api/src/centre_api/services/auth_api_service.py
+++ centre-api/src/centre_api/services/auth_api_service.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""auth admin functions"""
+"""auth admin functions."""
 import os
 
 import requests
