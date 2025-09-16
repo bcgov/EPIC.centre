b79ee1b3942cc4485c8b1d601006b8c23bd1a52a
Jad <jad.saad@aot-technologies.com>
2025-09-16 13:05:04 -0700

    Implements user search input field
    Adds a search input field for finding users.
    
    The field includes a search icon and allows users to
    search by name.

--- centre-web/src/components/AuthManagement/AllUsers/index.tsx
+++ centre-web/src/components/AuthManagement/AllUsers/index.tsx
@@ -1,3 +1,36 @@
+import { Button, Grid, Stack, TextField } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
 export const AllUsers = () => {
-  return <div>All Users</div>;
+  return (
+    <Grid container spacing={2} mt={"1em"}>
+      <Grid item xs={12}>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <TextField
+            variant="outlined"
+            placeholder="Search users by name"
+            InputProps={{
+              startAdornment: (
+                <SearchIcon
+                  sx={{ color: "#7B90A7", height: "30px", width: "30px" }}
+                />
+              ),
+            }}
+            sx={{
+              width: "400px",
+            }}
+          />
+          <Button
+            sx={{
+              width: "40px",
+              height: "40px",
+              minWidth: "40px",
+            }}
+          >
+            <SearchIcon sx={{ height: "30px", width: "30px" }} />
+          </Button>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
 };
