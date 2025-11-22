c00c520ef9b003c0d96ed5bbba00bf0508fef377
Jad <jad.saad@aot-technologies.com>
2025-11-21 16:32:35 -0800

    Run isort

--- centre-api/src/centre_api/services/user_service.py
+++ centre-api/src/centre_api/services/user_service.py
@@ -16,11 +16,7 @@ from collections import defaultdict
 
 from centre_api.enums.access_request_status import AccessRequestsStatusEnum
 from centre_api.enums.epic_app import (
-    APP_NAME_TO_CLIENT_NAME_MAP,
-    APP_NAME_TO_GROUP_MAP,
-    GROUP_TO_APP_NAME_MAP,
-    EpicAppClientName
-)
+    APP_NAME_TO_CLIENT_NAME_MAP, APP_NAME_TO_GROUP_MAP, GROUP_TO_APP_NAME_MAP, EpicAppClientName)
 from centre_api.models.access_requests import AccessRequests as AccessRequestsModal
 from centre_api.services.auth_api_service import AuthApiService
 from centre_api.utils.token_info import TokenInfo
