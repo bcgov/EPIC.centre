dfca9662de05e31f4b6154b890d72ec15270f60a
Jad <jad.saad@aot-technologies.com>
2025-11-26 16:52:07 -0800

    Remove print

--- centre-api/src/centre_api/resources/users.py
+++ centre-api/src/centre_api/resources/users.py
@@ -59,7 +59,6 @@ class UserByUsername(Resource):
         user = UserService.get_user_by_username(username)
         if not user:
             return {'message': 'User not found'}, HTTPStatus.NOT_FOUND
-        print(user)
         return UserSchema().dump(user), HTTPStatus.OK
 
     @staticmethod
