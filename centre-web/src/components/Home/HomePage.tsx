40b845c498bc42af88083d304b2610177afdec45
don-aot <don.varghese@aot-technologies.com>
2025-12-02 09:52:04 -0800

    CENTR-57:Home Page (#89)
    * CENTR-57:Home Page
    
    * CENTR-57:Home Page
    
    Linting issue fixes
    
    * CENTR-57:Home Page
    
    * CENTR-57:Home Page
    
    * CENTR-57:Home Page
    
    Linting issue fix
    
    * CENTR-57:Home Page

--- centre-web/src/components/Home/HomePage.tsx
+++ centre-web/src/components/Home/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Grid } from "@mui/material";
 
 const applications = [
   {
@@ -31,6 +31,8 @@ const applications = [
   },
 ];
 
+
+
 export default function HomePage() {
   return (
     <Box
@@ -46,8 +48,7 @@ export default function HomePage() {
         <Box
           sx={{
             margin: "0 auto",
-            paddingLeft: "10.4375em",
-            paddingRight: "15.4375em",
+            paddingLeft: { xs: "1em", sm: "2em", md: "3em", lg: "10.4375em" },
             position: "absolute",
             bottom: 0,
             paddingBottom: "0.5625em",
@@ -58,7 +59,7 @@ export default function HomePage() {
             sx={{
               color: "#292929",
               fontFamily: '"BC Sans", sans-serif',
-              fontSize: "2.25em",
+              fontSize: { xs: "1.5em", sm: "1.75em", md: "2em", lg: "2.25em" },
               fontStyle: "normal",
               fontWeight: 700,
               lineHeight: "normal",
@@ -73,120 +74,139 @@ export default function HomePage() {
       <Box
         sx={{
           margin: "0 auto",
-          paddingLeft: "10.4375em",
-          paddingRight: "15.4375em",
+          paddingLeft: { xs: "1em", sm: "2em", md: "3em", lg: "10.4375em" },
           position: "relative",
+          overflow: "hidden",
         }}
       >
-        <Box
-          sx={{
-            width: "66.66%",
-            position: "relative",
-            zIndex: 2,
-          }}
-        >
-          <Typography
+        <Grid container spacing={3} sx={{ position: "relative" }}>
+          <Grid
+            item
+            xs={12}
+            md={7}
+            lg={8}
             sx={{
-              color: "#292929",
-              fontFamily: '"BC Sans", sans-serif',
-              fontSize: "1.125em",
-              fontStyle: "normal",
-              fontWeight: 400,
-              lineHeight: "1.913em",
-              marginBottom: "2.5em",
+              position: "relative",
+              zIndex: 2,
             }}
           >
-            EPIC.centre gives you quick access to EAO applications, permission
-            controls, and important resources in one place.
-          </Typography>
+            <Typography
+              sx={{
+                color: "#292929",
+                fontFamily: '"BC Sans", sans-serif',
+                fontSize: { xs: "1em", sm: "1.0625em", lg: "1.125em" },
+                fontStyle: "normal",
+                fontWeight: 400,
+                lineHeight: "1.913em",
+                marginBottom: "2.5em",
+              }}
+            >
+              EPIC.centre gives you quick access to EAO applications, permission
+              controls, and important resources in one place.
+            </Typography>
 
-          <Typography
-            sx={{
-              color: "#292929",
-              fontFamily: '"BC Sans", sans-serif',
-              fontSize: "1em",
-              fontStyle: "normal",
-              fontWeight: 400,
-              lineHeight: "1.688em",
-              marginBottom: "1.5em",
-            }}
-          >
-            Based on your access level, EPIC.centre provides access to the
-            following applications:
-          </Typography>
+            <Typography
+              sx={{
+                color: "#292929",
+                fontFamily: '"BC Sans", sans-serif',
+                fontSize: { xs: "0.9375em", lg: "1em" },
+                fontStyle: "normal",
+                fontWeight: 400,
+                lineHeight: "1.688em",
+                marginBottom: "1.5em",
+              }}
+            >
+              Based on your access level, EPIC.centre provides access to the
+              following applications:
+            </Typography>
 
-          <Box
-            component="ul"
-            sx={{
-              margin: 0,
-              paddingLeft: "1.5em",
-              marginBottom: "1.5em",
-              listStyleType: "disc",
-            }}
-          >
-            {applications.map((app) => (
-              <Box component="li" key={app.name} sx={{ marginBottom: "0.25em" }}>
-                <Typography
-                  component="span"
-                  sx={{
-                    color: "#292929",
-                    fontFamily: '"BC Sans", sans-serif',
-                    fontSize: "1em",
-                    fontStyle: "normal",
-                    fontWeight: 400,
-                    lineHeight: "1.688em",
-                  }}
-                >
-                  <Box component="span" sx={{ fontWeight: 600 }}>
-                    {app.name}
-                  </Box>{" "}
-                  - {app.description}
-                </Typography>
-              </Box>
-            ))}
-          </Box>
-
-          <Typography
-            sx={{
-              color: "#292929",
-              fontFamily: '"BC Sans", sans-serif',
-              fontSize: "1em",
-              fontStyle: "normal",
-              fontWeight: 400,
-              lineHeight: "1.688em",
-            }}
-          >
-            Search all project information and documents directly through{" "}
-            <Box component="span" sx={{ fontWeight: 600 }}>
-              Document Search.
+            <Box
+              component="ul"
+              sx={{
+                margin: 0,
+                paddingLeft: "1.5em",
+                marginBottom: "1.5em",
+                listStyleType: "disc",
+              }}
+            >
+              {applications.map((app) => (
+                <Box component="li" key={app.name} sx={{ marginBottom: "0.25em" }}>
+                  <Typography
+                    component="span"
+                    sx={{
+                      color: "#292929",
+                      fontFamily: '"BC Sans", sans-serif',
+                      fontSize: { xs: "0.9375em", lg: "1em" },
+                      fontStyle: "normal",
+                      fontWeight: 400,
+                      lineHeight: "1.688em",
+                    }}
+                  >
+                    <Box component="span" sx={{ fontWeight: 600 }}>
+                      {app.name}
+                    </Box>{" "}
+                    - {app.description}
+                  </Typography>
+                </Box>
+              ))}
             </Box>
-          </Typography>
-        </Box>
 
-        <Box
-          sx={{
-            marginTop: "-5em",
-            marginLeft: "50em",
-            width: "auto",
-            pointerEvents: "none",
-            zIndex: 1,
-            overflow: "hidden",
-          }}
-        >
-          <Box
-            component="img"
-            src="/home_page_illustration.png"
-            alt="Workspace illustration"
+            <Typography
+              sx={{
+                color: "#292929",
+                fontFamily: '"BC Sans", sans-serif',
+                fontSize: { xs: "0.9375em", lg: "1em" },
+                fontStyle: "normal",
+                fontWeight: 400,
+                lineHeight: "1.688em",
+              }}
+            >
+              Search all project information and documents directly through{" "}
+              <Box component="span" sx={{ fontWeight: 600 }}>
+                Document Search.
+              </Box>
+            </Typography>
+          </Grid>
+
+          <Grid
+            item
+            xs={12}
+            md={5}
+            lg={4}
             sx={{
-              width: "32.8125em",
-              height: "24.5625em",
-              aspectRatio: "175/131",
-              objectFit: "cover",
+              display: "flex",
+              alignItems: { xs: "center", md: "flex-start" },
+              justifyContent: { xs: "right", md: "flex-start" },
+              paddingTop: { xs: "2em", md: "0" },
+              marginTop: { 
+                xs: 0,        
+                sm: "5em",   
+                md: "20em",  
+                lg: "20em"    
+              },
               position: "relative",
               zIndex: 1,
+              overflow: "visible",
+              marginLeft: { xs: 0, md: "-5em", lg: "-10em" },
             }}
-          />
-        </Box>
+          >
+            <Box
+              component="img"
+              src="/home_page_illustration.png"
+              alt="Workspace illustration"
+              sx={{
+                width: "100%",
+                maxWidth: { xs: "100%", sm: "28em", md: "26em", lg: "32.8125em" },
+                height: "auto",
+                aspectRatio: "175/131",
+                objectFit: "contain",
+                position: "relative",
+                pointerEvents: "none",
+                marginLeft: { xs: 0, md: "-2em", lg: 0 },
+              }}
+            />
+          </Grid>
+        </Grid>
       </Box>
 
     </Box>
