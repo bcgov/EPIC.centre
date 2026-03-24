4a2be5a565edd94bbe1145aab87dd9a226006029
saravanpa-aot <saravankumar.pa@aot-technologies.com>
2026-03-23 22:29:43 -0700

    fixing the alembic version

--- centre-api/migrations/versions/e55517dc0fd8_add_application_urls_table.py
+++ centre-api/migrations/versions/e55517dc0fd8_add_application_urls_table.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'e55517dc0fd8'
-down_revision = 'b2c3d4e5f6a7'
+down_revision = 'c3d4e5f6a7b8'
 branch_labels = None
 depends_on = None
 
